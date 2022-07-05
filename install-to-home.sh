79d3923b8d6e96476bd972593d3a3863297b4f31
mrcrsr <mrcrsr@posteo.de>
2022-07-05 20:15:45 +0200

    Install: Using non-aliasing command

--- install-to-home.sh
+++ install-to-home.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-\cp -v filetype.sh                 "$HOME/bin/filetype"
-\cp -v git-list-repo-states.sh     "$HOME/bin/git-list-repo-states"
-\cp -v git-list-file-extensions.sh "$HOME/bin/git-list-file-extensions"
-\cp -v git-fetch-repos.sh          "$HOME/bin/git-fetch-repos"
+command cp -vf filetype.sh                 "$HOME/bin/filetype"
+command cp -vf git-list-repo-states.sh     "$HOME/bin/git-list-repo-states"
+command cp -vf git-list-file-extensions.sh "$HOME/bin/git-list-file-extensions"
+command cp -vf git-fetch-repos.sh          "$HOME/bin/git-fetch-repos"
 
